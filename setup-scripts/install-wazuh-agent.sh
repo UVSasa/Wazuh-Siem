87c2815568556ce7a491c9d0f6ce98da13abf7be
CySasa <savionamari@icloud.com>
2025-04-23 10:22:20 -0500

    Create install-wazuh-agent.sh

---
+++ setup-scripts/install-wazuh-agent.sh
@@ -0,0 +1,7 @@
+# Download and install the wazuh agent
+#This will download the Windows version of the Wazuh agent. Check offical website for a different OS
+#Using CMD run the below command:
+wazuh-agent-4.11.2-1.msi /q WAZUH_MANAGER="MANAGER IP"
+
+#Or you can run the below command using powershell on windows:
+.\wazuh-agent-4.11.2-1.msi /q WAZUH_MANAGER="MANAGER IP"
