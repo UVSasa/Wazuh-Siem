4cf46225e55cacad2ee19f28442396d9586a01a0
CySasa <savionamari@icloud.com>
2025-04-23 10:16:41 -0500

    Update install-wazuh.sh

--- setup-scripts/install-wazuh.sh
+++ setup-scripts/install-wazuh.sh
@@ -1,1 +1,6 @@
-instalation command for wazuh and agents
+#Download and run the wazuh instalation assistant for wazuh manager and agents
+#This will install the Wazuh manager, indexer, and dashboard
+curl -sO https://packages.wazuh.com/4.11/wazuh-install.sh && sudo bash ./wazuh-install.sh -a
+
+# You should now be able to access the wazuh web interface using https://<WAZUH_DASHBOARD_IP_ADDRESS> and your credentials you
+# were given when the installation was complete
